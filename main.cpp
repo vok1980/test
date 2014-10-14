177b3a0e96bf139a2efe8697b66ff5ba2a86d3bb
Vyacheslav Koshcheyev <vyacheslav.koshcheyev@akcp.com>
2014-10-14 13:06:19 +0700

    add boost::variant container type check

--- main.cpp
+++ main.cpp
@@ -11,10 +11,31 @@
 typedef boost::variant < uint32_t, float > t_data;
 
 
+template <typename Type>
+struct TypeCheckVisitor : public boost::static_visitor<bool>
+{
+    template < typename TypeOther >
+    result_type operator()(TypeOther &data) const { return boost::is_same<Type, TypeOther>::value; }
+};
+
+
+template <typename Type>
+bool IsContainsType(t_data &val)
+{
+	return boost::apply_visitor(TypeCheckVisitor<Type>(), val);
+}
+
+
 int main()
 {
 	t_data var00(0.4f);
 	t_data var01((uint32_t)57);
+
+	assert ( IsContainsType<float>(var00) );
+	assert ( !IsContainsType<double>(var00) );
+	assert ( !IsContainsType<uint32_t>(var00) );
+
+	assert ( IsContainsType<uint32_t>(var01) );
 
 	t_data var02, var03;
 
